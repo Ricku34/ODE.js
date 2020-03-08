f60e3e92e387c7f1b2ea393f9d10fc37f1bae299
eric <eric.hasdenteufel@geomatys.com>
2020-03-08 17:11:41 +0100

    V1.1.0 : add WASM features.

--- tests/SpaceCollide.js
+++ tests/SpaceCollide.js
@@ -1,30 +1,35 @@
 var assert = require('assert');
 var ODE = require('../lib/libode.js');
 assert(ODE);
-var space = new ODE.Space.Hash();
-assert(space);
-var sph1 = ODE.Geom.createSphere(3);
-var sph2 = ODE.Geom.createSphere(3);
-var group = space.createSimpleGroup();
-assert(sph1);
-assert(sph2);
-assert(group);
-sph1.setPosition(-1,0,1);
-sph2.setPosition(1,0,0);
-group.add(sph1);
-group.add(sph2);
-var collide = false;
-space.collide(function(g1,g2)
-{
-	collide = true;
-})
-assert(!collide);
-ODE.Geom.collide(sph1,sph2,1,function(contact)
-{
-	collide = true;
-	console.log(contact);
+ODE.readyPromise.then(function() {
+	assert(ODE.ready);
+    console.log("ODE API is ready! ");
+    var world = new ODE.World();
+	var space = new ODE.Space.Hash();
+	assert(space);
+	var sph1 = ODE.Geom.createSphere(3);
+	var sph2 = ODE.Geom.createSphere(3);
+	var group = space.createSimpleGroup();
+	assert(sph1);
+	assert(sph2);
+	assert(group);
+	sph1.setPosition(-1,0,1);
+	sph2.setPosition(1,0,0);
+	group.add(sph1);
+	group.add(sph2);
+	var collide = false;
+	space.collide(function(g1,g2)
+	{
+		collide = true;
+	})
+	assert(!collide);
+	ODE.Geom.collide(sph1,sph2,1,function(contact)
+	{
+		collide = true;
+		console.log(contact);
+	});
+	assert(collide);
+	//console.log(ODE.Geom.getAABB(sph1))
+	//console.log(ODE.Geom.getSpaceAABB(sph2))
+	console.log('OK !');
 });
-assert(collide);
-//console.log(ODE.Geom.getAABB(sph1))
-//console.log(ODE.Geom.getSpaceAABB(sph2))
-console.log('OK !');
